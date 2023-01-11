5fdcd1aa300b58c6121aecfdbb1d84dfe8da9a5b
Arya <32606430+Finura99@users.noreply.github.com>
2023-01-11 17:59:12 +0100

    Table version 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import data from "./mock.json";
 
-function App() {
+const App = () => {
+
+  const [contact, setContacts] = useState(data);
+  //we use data from json and storing it in useState using the useState hook.
+  //this state is useable in the contacts variable. The setContacts function updates the state.
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "app-container">
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>firstName</th>
+            <th>LastName</th>
+            <th>createdAt</th>
+            <th>Salary</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contact.map((contacts) => (
+          <tr>
+            <td>{contacts.id}</td>
+            <td>{contacts.firstName}</td>
+            <td>{contacts.LastName}</td>
+            <td>{contacts.createdAt}</td>
+            <td>{contacts.Salary}</td>
+          </tr>
+          ))}
+          
+        </tbody>
+      </table>
     </div>
   );
 }
 
 export default App;
+
+
+
